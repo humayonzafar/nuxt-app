5e542ce2468932bb85590837500be61ca5427ac4
Humayon Zafar <humayonhere@gmail.com>
2022-01-24 04:04:34 +0500

    Fixed eslint issues

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,10 +1,9 @@
-import { mount } from '@vue/test-utils'
+import {mount} from "@vue/test-utils";
 import Hero from "~/components/Hero";
 
-describe('mounted hero', () => {
+describe("mounted hero", () => {
   const wrapper = mount(Hero);
-
-  test('does a wrapper exist', () => {
+  test("does a wrapper exist", () => {
     expect(wrapper.exists()).toBe(true)
   })
 })
